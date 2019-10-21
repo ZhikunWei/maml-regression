8a5802717ed283eba98bd968e0b79ecd939e1878
ZhikunWei <1045525044@qq.com>
2019-10-21 17:25:12 +0800

    update

--- maml_from_pkl.py
+++ maml_from_pkl.py
@@ -3,9 +3,10 @@ import pickle
 import torch
 from maml_regression import MAML_Regression
 from maml_regression import sample_data
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    with open('log/itr299.pkl', 'rb') as f:
+    with open('log/itr2999.pkl', 'rb') as f:
         maml = pickle.load(f)
     maml.plot_figure = True
     new_task_x, new_task_y, test_amp, test_pha = sample_data(1, 10)
@@ -15,5 +16,6 @@ if __name__ == '__main__':
     new_task_test_x, new_task_test_y = torch.tensor(new_task_test_x, dtype=torch.float32), torch.tensor(new_task_test_y,
                                                                                                         dtype=torch.float32)
     maml.meta_testing(new_task_x, new_task_y, new_task_test_x, new_task_test_y)
+    plt.show()
 
 
